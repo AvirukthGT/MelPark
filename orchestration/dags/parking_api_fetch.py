712d585385654739ccf557084fcd2e636a4c48f1
avirukthgt <avirukth@gmail.com>
2026-01-15 16:36:10 +1100

    feat: implement Postgres loading logic for parking data
    - Create processing/postgres_loader.py with idempotency logic
    
    - Update DAG to link ingestion tasks to loading tasks

--- orchestration/dags/parking_api_fetch.py
+++ orchestration/dags/parking_api_fetch.py
@@ -2,14 +2,16 @@ import sys
 import os
 
 # 1. Add the project root to Python's search path
+# I need to do this so Airflow can find my 'ingestion' module, which is in the parent directory.
 sys.path.append("/opt/airflow")
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 from ingestion.parking_api import ingest_api_data 
-
-# Note: We removed the '?limit=20' from these URLs because the script adds it manually
-# Updated Source List using the EXPORTS endpoint
+from processing.postgres_loader import load_to_postgres
+# Note: I removed the '?limit=20' from these URLs because the script adds it manually
+# I'm defining my API sources here. This EXPORTS endpoint is great because it gives me everything 
+# without needing to paginate.
 API_SOURCES = [
     {
         "name": "parking_sensors", 
@@ -33,16 +35,20 @@ API_SOURCES = [
     }
 ]
 
+# I'm setting up the DAG here. I set catchup=False because I don't want to run a bunch of old 
+# backfills if I pause the DAG.
 with DAG(
-    dag_id='melbourne_parking_ingestion',
+    dag_id='melbourne_parking_elt',
     start_date=datetime(2026, 1, 14),
     schedule_interval='@daily',
     catchup=False,
-    tags=['ingestion', 'melbourne']
+    tags=['melbourne', 'elt']
 ) as dag:
 
     for source in API_SOURCES:
-        PythonOperator(
+        
+        # Task 1: Get Data
+        task_ingest = PythonOperator(
             task_id=f"ingest_{source['name']}",
             python_callable=ingest_api_data,
             op_kwargs={
@@ -50,3 +56,15 @@ with DAG(
                 "filename_prefix": source['name']
             }
         )
+
+        # Task 2: Load Data
+        task_load = PythonOperator(
+            task_id=f"load_{source['name']}",
+            python_callable=load_to_postgres,
+            op_kwargs={
+                "filename_prefix": source['name']
+            }
+        )
+
+        # Dependency
+        task_ingest >> task_load
